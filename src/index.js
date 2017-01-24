0c778241dc42a51fb17f583636f786462ebaed69
DanZeuss <danvc@zeuss.com>
2017-01-24 18:16:41 -0200

    Events with React

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import App  from './App';
 import Button  from './Button';
 import CustomValidation from './CustomValidation' 
+import Events from './Events' 
 
 ReactDOM.render(
   // to test each example, simple modify the name of the element (i.e. App, Button, CustomValidation, etc)
-  <CustomValidation />,
+  <Events />,
   document.getElementById('root')
 );
