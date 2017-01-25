32eaa16c5b3c16ad64fd86bd6e10e57fac8267ad
DanZeuss <danvc@zeuss.com>
2017-01-25 00:21:23 -0200

    Control when a component is updated or should update, will receive props, etc

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import CustomValidation from './CustomValidation'
 import Events from './Events' 
 import GetComponentReference from './GetComponentReference'
 import ComponentLifeCycle from './ComponentLifeCycle'
+import ControlComponentUpdates from './ControlComponentUpdates'
 
 ReactDOM.render(
   // to test each example, simple modify the name of the element (i.e. App, Button, CustomValidation, etc)
-  <ComponentLifeCycle />,
+  <ControlComponentUpdates />,
   document.getElementById('root')
 );
